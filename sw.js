91b22876ed33f0177e71e16bca220d7e4763a3e2
erofly83-arch <erofly83@gmail.com>
2026-03-08 15:45:38 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'price-manager-shell-v1';
+const SHELL_CACHE = 'price-manager-shell-v2';
 const CDN_CACHE   = 'price-manager-cdn-v1';
 const FONT_CACHE  = 'price-manager-fonts-v1';
 const BASE        = '/pricemanager';
